553b9a0974d4777a2564a6b2573f946447631545
Tim Sogard <src@timsogard.com>
2019-11-17 18:32:21 -0500

    Move Pinboard-Auth header validation into a handler

--- auth.go
+++ auth.go
@@ -7,21 +7,34 @@ import (
 	"strings"
 )
 
+// Ensure's an appropriately formed Pinboard-Auth header is present. Cannot assert
+// on format of API keys since the format has changed over the years without expiring
+// previous format keys. Auth against the Pinboard API is _explicitly_ not done here
+// to avoid adding an extra API call to every request. Auth issues are handled by
+// pinboard.get
+func requireAuth(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Pinboard-Auth")
+		authInfo := strings.Split(auth, ":")
+		if len(authInfo) != 2 {
+			msg := fmt.Errorf("Pinboard-Auth header should be in the form <username>:<token>")
+			http.Error(w, msg.Error(), http.StatusUnauthorized)
+			return
+		}
+		username := authInfo[0]
+		token := authInfo[1]
+		if len(username) < 1 || len(token) < 1 {
+			msg := fmt.Errorf("Pinboard-Auth header should be in the form <username>:<token>")
+			http.Error(w, msg.Error(), http.StatusUnauthorized)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
 func authedPinboard(w http.ResponseWriter, r *http.Request) *pinboard.Pinboard {
 	auth := r.Header.Get("Pinboard-Auth")
 	authInfo := strings.Split(auth, ":")
-	if len(authInfo) != 2 {
-		msg := fmt.Errorf("Pinboard-Auth header should be in the form <username>:<token>")
-		http.Error(w, msg.Error(), http.StatusUnauthorized)
-		return nil
-	}
-	username := authInfo[0]
-	token := authInfo[1]
-	if len(username) < 1 || len(token) < 1 {
-		msg := fmt.Errorf("Pinboard-Auth header should be in the form <username>:<token>")
-		http.Error(w, msg.Error(), http.StatusUnauthorized)
-		return nil
-	}
 
 	p := &pinboard.Pinboard{User: authInfo[0], Token: authInfo[1]}
 	return p
--- server.go
+++ server.go
@@ -10,7 +10,8 @@ func main() {
 	log.Println("tagger backend")
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/login", loginHandler)
+	mux.Handle("/login", requireAuth(http.HandlerFunc(loginHandler)))
+	mux.Handle("/posts/dates", requireAuth(http.HandlerFunc(postDatesHandler)))
 
 	handler := cors.Default().Handler(mux)
 	http.ListenAndServe(":4567", handler)
